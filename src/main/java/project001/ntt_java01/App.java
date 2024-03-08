3f84b3753f1397e53b6d9ff1d9be064b4ba8373c
ravikumar <ravikelakam@gmail.com>
2024-03-08 12:49:19 +0530

    first commit

---
+++ src/main/java/project001/ntt_java01/App.java
@@ -0,0 +1,13 @@
+package project001.ntt_java01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "calling application.. " );
+    }
+}
---
+++ src/test/java/project001/ntt_java01/AppTest.java
@@ -0,0 +1,20 @@
+package project001.ntt_java01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+         System.out.println("testing ntt java 01 project");
+    }
+}
